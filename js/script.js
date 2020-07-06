a37a5333463e712787302796c05cd053e31ffd11
E CC K <65670705+ehrencc@users.noreply.github.com>
2020-07-06 14:37:02 +0200

    RNG colors
    added RNG color hexes instead of using an array

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ var colors = ["red","silver","teal","orange","pink","gold",
 function changeBG(){
   document.body.style.backgroundColor= colors[Math.floor(Math.random()*colors.length)]
 }; 
+
+//function random hex generator to be added to body background color.
+function RNGchangeBG(){
+  var hash = "#";
+  
+  var colorHex = Math.floor(Math.random()*16777125).toString(16);
+  
+  document.body.style.backgrondColor = hash + colorHex ;
+};
